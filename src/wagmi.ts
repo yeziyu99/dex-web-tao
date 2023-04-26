dba7604ee469b863050dde8236ea8c47c2b02e34
Allen Zhang <ztao165@163.com>
2023-04-25 19:51:58 -0700

    Add Foundry, localhost chains, and jsonRpcProvider

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,15 +1,22 @@
 import { configureChains, createClient } from 'wagmi'
-import { goerli, mainnet } from 'wagmi/chains'
+import { goerli, mainnet, foundry, localhost } from 'wagmi/chains'
 import { CoinbaseWalletConnector } from 'wagmi/connectors/coinbaseWallet'
 import { InjectedConnector } from 'wagmi/connectors/injected'
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask'
 import { WalletConnectLegacyConnector } from 'wagmi/connectors/walletConnectLegacy'
 import { publicProvider } from 'wagmi/providers/public'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 const { chains, provider, webSocketProvider } = configureChains(
-  [mainnet, ...(import.meta.env?.MODE === 'development' ? [goerli] : [])],
+  [goerli, foundry, localhost],
   [
     publicProvider(),
+    jsonRpcProvider({
+      rpc: (localhost) => ({
+        http: `127.0.0.1:8545`,
+        webSocket: `ws://127.0.0.1:8545`,
+      }),
+    }),
   ],
 )
 
